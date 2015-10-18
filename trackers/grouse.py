0caf4a638de4b0c69733f01dd682d31830046a3f
Greg Hasseler <ghasseler@gmail.com>
2015-10-17 21:28:52 -0400

    Misc. whitespace fixes.

--- trackers/grouse.py
+++ trackers/grouse.py
@@ -35,54 +35,50 @@ class Grouse(tracker.Tracker):
         self.ambient_dim = ambient_dim
         self.rank = rank
         self.step = step
-        
+
         # Initialize our subspace estimate to a random orthogonal matrix
-        self.U = np.linalg.qr(np.random.rand(ambient_dim, rank))[0]
-        
-#        print("shape of U: {}".format(self.U.shape))
-        
+        self.U = np.linalg.qr(np.random.rand(ambient_dim, rank))[0]        
+
     def consume(self, ob_vec, sample_vec, step=None):
         if step is None:
             step = self.step
-        
+
         # Find our projection into the subspace
         projection = self._project(ob_vec, sample_vec)
-        
+
         # Find the imputed measurement residual
         row_indices = np.nonzero(sample_vec)[0]        
         imputed_measurement = self.U @ projection
         residual = ob_vec[row_indices,:] - imputed_measurement[row_indices,:]
-        
-#        print("Imputed measurement residual: {}".format(residual))
-        
+
         sigma = np.linalg.norm(residual) * np.linalg.norm(imputed_measurement)
-        
+
         normalized_imputed_measurement = imputed_measurement / np.linalg.norm(imputed_measurement)
         normalized_residual = residual / np.linalg.norm(residual)
         normalized_projection = projection.T / np.linalg.norm(projection)
-        
+
         lhs_inner_update = (np.cos(sigma * step) - 1) * normalized_imputed_measurement
         rhs_inner_update = np.sin(sigma * step) * normalized_residual
-             
         rhs = (lhs_inner_update + rhs_inner_update) @ normalized_projection
+
         self.U = self.U + rhs        
-    
+
     def _project(self, ob_vec, sample_vec):
         """Find the projection of the observation vector (ob_vec) into the subspace.
         Keyword arguments:
         ob_vec: observation vector with possibly missing data. Noise entries should be marked in sample_vec
         sample_vec: vector indicating missing data entries in ob_vec. Entries corresponding with ob_vec should be 1 where data was recorded and 0 where data was missing
         """
-        
+    
         # Take only the rows of U that correspond to attributes we have observed
         row_indices = np.nonzero(sample_vec)[0]        
         U_samp = self.U[row_indices,:]
         ob_vec_samp = ob_vec[row_indices,:]
-                
+  
 #        print("Shape of U_samp: {}".format(U_samp.shape))
 #        print("Shape of ob_vec_samp: {}".format(ob_vec_samp.shape))
-        
+    
         # Compute the weights
         projection = np.linalg.lstsq(U_samp, ob_vec_samp)[0]
-        
+
         return projection
